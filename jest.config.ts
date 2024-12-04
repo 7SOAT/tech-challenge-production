fe909f74455d6121ead6bad7eb503a937225e7c8
Luiz - Personal <luizgmaker@gmail.com>
2024-12-03 23:17:30 -0300

    fixed config file

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'jest';
 
 const config: Config = {
+  rootDir: '.',
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.test.ts', '**/test/features/**/*.feature'],
