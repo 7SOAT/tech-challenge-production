b0c4cf59950e3f61603752aa949f9ab536e01d05
Luiz - Personal <luizgmaker@gmail.com>
2024-12-03 23:51:26 -0300

    added sonarq integration

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
-  moduleFileExtensions: ['ts', 'js'],
+  moduleFileExtensions: ['ts', 'js'],  
 };
 
 export default config;
