3969cb6df7c75e92c66e4e404abf6dba7ed402ca
Luiz - Personal <luizgmaker@gmail.com>
2024-12-03 23:07:55 -0300

    fixed path

--- test/entity/steps/order.entity.test.ts
+++ test/entity/steps/order.entity.test.ts
@@ -1,6 +1,7 @@
+import { Order } from "core/entities/order";
 import { UUID } from "crypto";
 import { defineFeature, loadFeature } from "jest-cucumber";
-import { Order } from "../../../src/core/entities/order";
+
 
 const feature = loadFeature('./test/entity/features/order.entity.feature');
 
